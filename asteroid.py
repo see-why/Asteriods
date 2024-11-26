455bcab1aae72dcd63badc5634f979f728d9a47a
Iyadi Cyril Osita <iyadicy@gmail.com>
2024-11-26 15:22:39 -0500

    split hit asteriod to smaller asteroids

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 import pygame
+import random
 
 from circleshape import CircleShape
+from constants import ASTEROID_MIN_RADIUS
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -12,3 +14,25 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
        self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        rotate_angle = random.uniform(20, 50)
+        first_rotate_vector = self.velocity.rotate(rotate_angle)
+        second_rotate_vector = self.velocity.rotate(-rotate_angle)
+
+        other_radius = self.radius - ASTEROID_MIN_RADIUS
+        small_asteroid = Asteroid(self.position.x, self.position.y, ASTEROID_MIN_RADIUS)
+        small_asteroid.velocity = first_rotate_vector * 2.0
+        other_asteroid = Asteroid(self.position.x, self.position.y, other_radius)
+        other_asteroid.velocity = second_rotate_vector * 1.2
+
+
+
+        
+
+
