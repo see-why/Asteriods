d3d0d4b9f5e2b5f34926c187d50c97c52f2eea48
Iyadi Cyril Osita <iyadicy@gmail.com>
2024-12-19 23:12:15 -0500

    adjust sound volume

--- dj.py
+++ dj.py
@@ -3,5 +3,8 @@ import pygame
 class Dj:
     pygame.mixer.init() 
     shoot_mixer = pygame.mixer.Sound('sounds/shoot.ogg')
-    asteroid_explosion_mixer = pygame.mixer.Sound("sounds/player_explodes.ogg")
-    player_explosion_mixer = pygame.mixer.Sound("sounds/asteroid_explodes.wav")
+    shoot_mixer.set_volume(0.1)
+    player_explosion_mixer = pygame.mixer.Sound("sounds/player_explodes.ogg")
+    player_explosion_mixer.set_volume(0.1)
+    asteroid_explosion_mixer = pygame.mixer.Sound("sounds/asteroid_explodes.wav")
+    asteroid_explosion_mixer.set_volume(0.1)
