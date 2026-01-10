d8c701c32a2d4ed49d2ad741a34592237f4bdf98
Osita Cyril Iyadi <Iyadicy@gmail.com>
2026-01-10 10:00:00 -0500

    Add different colored asteroid types with varying properties

--- asteroid.py
+++ asteroid.py
@@ -8,9 +8,39 @@ class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
         self.rotation = 0
+        self.asteroid_type = self.determine_type(radius)
+        self.color = self.get_color()
+        self.health_multiplier = self.get_health_multiplier()
+    
+    def determine_type(self, radius):
+        """Determine asteroid type based on radius"""
+        if radius >= ASTEROID_MIN_RADIUS * 3:
+            return "large"
+        elif radius >= ASTEROID_MIN_RADIUS * 2:
+            return "medium"
+        else:
+            return "small"
+    
+    def get_color(self):
+        """Get color based on asteroid type"""
+        colors = {
+            "large": "red",
+            "medium": "orange",
+            "small": "white"
+        }
+        return colors.get(self.asteroid_type, "white")
+    
+    def get_health_multiplier(self):
+        """Get health multiplier based on type"""
+        multipliers = {
+            "large": 2.0,
+            "medium": 1.5,
+            "small": 1.0
+        }
+        return multipliers.get(self.asteroid_type, 1.0)
 
     def draw(self, screen):
-        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+        pygame.draw.circle(screen, self.color, self.position, self.radius, 2)
 
     def update(self, dt):
        self.position += self.velocity * dt
